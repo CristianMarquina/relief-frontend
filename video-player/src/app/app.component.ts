91cf64334b870b5058bbdd060a54ff70d24e655a
Cristian Marquina <cristian@3dlinkweb.com>
2025-01-11 17:27:47 -0400

    add history component

--- video-player/src/app/app.component.ts
+++ video-player/src/app/app.component.ts
@@ -23,8 +23,25 @@ import { HistoryComponent } from './components/history/history.component';
 export class AppComponent {
   title = 'video-player';
   videoUrl: string = '';
+  //historyList: { name: string; url: string }[] = [];
+  historyList: { name: string; url: string }[] = [
+    /*{ name: 'Video 1', url: 'https://www.youtube.com/watch?v=OxnHWPtiBOI' },
+    {
+      name: 'Video 2',
+      url: 'https://www.youtube.com/watch?v=_t8HWppY4T0&list=PL5gy95X4kdAhllolA8bLRxONt3RTBs8ZD&index=2',
+    },*/
+  ];
   onVideoAdded(e: SearchdData) {
     console.log(e);
     this.videoUrl = e.url;
+    this.historyList.push({ name: e.name ? e.name : e.url, url: e.url });
+  }
+
+  onHistorySelected(e: SearchdData) {
+    this.videoUrl = e.url;
+  }
+
+  onHistoryDeleted(index: number) {
+    this.historyList.splice(index, 1);
   }
 }
--- video-player/src/app/components/history/history.component.ts
+++ video-player/src/app/components/history/history.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { SearchdData } from '../searchbar/searchbar.component';
 
 @Component({
   selector: 'history',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './history.component.html',
-  styleUrl: './history.component.css',
+  styleUrls: ['./history.component.css'],
 })
-export class HistoryComponent {}
+export class HistoryComponent {
+  @Input() historyList: { name: string; url: string }[] = [];
+  @Output() selectHistory = new EventEmitter<SearchdData>();
+  @Output() deleteHistory = new EventEmitter<number>();
+
+  onSelectHistory(url: string) {
+    this.selectHistory.emit({ url: url, name: url });
+  }
+
+  onDeleteHistory(index: number) {
+    this.deleteHistory.emit(index);
+  }
+
+  truncateUrl(url: string, maxLength: number = 50): string {
+    return url.length > maxLength ? url.slice(0, maxLength) + '...' : url;
+  }
+}
