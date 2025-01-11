7873631f554cd14f81123b93c18f3c8d30ad176a
Cristian Marquina <cristian@3dlinkweb.com>
2025-01-11 12:43:17 -0400

    generate the components

---
+++ video-player/src/app/video-view/video-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video-view',
+  imports: [],
+  templateUrl: './video-view.component.html',
+  styleUrl: './video-view.component.css'
+})
+export class VideoViewComponent {
+
+}
